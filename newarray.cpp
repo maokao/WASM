c30e15802e1add4953b4542c2393b27b1d794d49
MaoKao <maokao25@gmail.com>
2019-07-16 11:35:27 +0800

    Add files via upload

---
+++ newarray.cpp
@@ -0,0 +1,21 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    int *getArray(int n)
+    {
+        int* newArray = new int[n];
+        for(int i=0; i<n; i++)
+        {
+            newArray[i]=i;
+        }
+
+        //return &newArray[0];
+        return newArray;
+    }
+ 
+
+#ifdef __cplusplus
+}
+#endif
